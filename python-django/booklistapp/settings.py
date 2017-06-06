de6f7a7158ec32b8503aebba8ba00e12dc35008b
Jan Vlcek <vlki@vlki.cz>
2017-06-06 23:32:37 +0200

    Setting up django model

--- python-django/booklistapp/settings.py
+++ python-django/booklistapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'booklistapp_public.apps.BooklistappPublicConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- python-django/booklistapp_public/models.py
+++ python-django/booklistapp_public/models.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = '"books"'
+
+    def __str__(self):
+        return str(self.id) + '-' + self.title
