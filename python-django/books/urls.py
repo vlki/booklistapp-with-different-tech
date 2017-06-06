3d35b8e1184dbf69af89f77a6dda08716ecb9485
Jan Vlcek <vlki@vlki.cz>
2017-06-06 23:48:19 +0200

    Listing books, basics of create and delete

--- python-django/books/urls.py
+++ python-django/books/urls.py
@@ -7,4 +7,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^delete-book/(?P<book_id>[0-9]+)$', views.delete, name='delete'),
+    url(r'^create-book$', views.create, name='create'),
 ]
--- python-django/books/views.py
+++ python-django/books/views.py
@@ -4,5 +4,15 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Book
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    books = Book.objects.all()
+    return render(request, 'books/index.html', {'books': books})
+
+def delete():
+    return HttpResponse('TBD')
+
+def create():
+    return HttpResponse('TBD')
