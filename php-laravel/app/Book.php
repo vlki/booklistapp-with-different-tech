ace301c2569ee2a0c51c397292b0fa1660e1823f
Jan Vlcek <vlki@vlki.cz>
2017-06-06 13:32:07 +0200

    Static books list

---
+++ php-laravel/app/Book.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+class Book
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+    ];
+}
---
+++ php-laravel/resources/views/books.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Booklistapp in PHP and Laravel</title>
+  </head>
+  <body>
+    <h1>Books</h1>
+
+    <ul>
+      <li>Book</li>
+    </ul>
+
+    <form action="add-book.php" method="POST">
+      <input type="text" name="title">
+      <input type="submit" value="Add">
+    </form>
+  </body>
+</html>
